a3d4a46f16d25e84d157f889402e61b9ae6763d1
nandhinis29 <nandhinis29.1994@gmail.com>
2022-04-09 11:46:01 -0700

    prince done hid codding

--- src/org/sample/GitProgram.java
+++ src/org/sample/GitProgram.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class GitProgram {
+	private void add1() {
+		System.out.println("add1");
+	}
 	public static void main(String[] args) {
 		System.out.println("done");
 	}
