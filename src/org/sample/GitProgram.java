98ead23785cf54af56e1b59094f6ca297ff52e92
nandhinis29 <nandhinis29.1994@gmail.com>
2022-04-09 11:54:37 -0700

    felix done his codding

--- src/org/sample/GitProgram.java
+++ src/org/sample/GitProgram.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class GitProgram {
+	private void add2() {
+	System.out.println("add2");
+	}
 	public static void main(String[] args) {
 		System.out.println("done");
 	}
